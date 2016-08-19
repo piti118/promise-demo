bb1631a9e3044fdaf84daa92cfa723d50760ce6f
Piti <piti118@gmail.com>
2016-08-19 14:28:11 +0700

    slowadd

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.get('/fastadd/:number', (req, res) => {
   res.json(parseInt(req.params.number, 10) * 2);
 });
 
+app.get('/slowadd/:number', (req, res) => {
+  setTimeout(() => {
+    res.json(parseInt(req.params.number, 10) * 2);
+  }, 1000);
+});
+
+
 const port = 9999;
 
 app.listen(port, (err, res) => {
